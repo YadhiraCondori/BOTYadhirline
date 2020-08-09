7e150c51b427cb67664a24315de6891a5e18fcab
Yadhirline09 <69084308+Yadhirline09@users.noreply.github.com>
2020-08-09 15:19:43 -0500

    envio de version 3.0

--- BOTYadhirline-src/Bots/EchoBot.cs
+++ BOTYadhirline-src/Bots/EchoBot.cs
@@ -13,20 +13,19 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"));
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and Welcome");
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hola y Bienvenido!"));
                 }
             }
         }
     }
 }
+
